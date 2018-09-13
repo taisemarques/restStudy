0b9eb574fb8e989faa117845985d75458867e5f1
taisemarques <taise.marques@hotmail.com>
2018-09-13 12:34:22 -0300

    Add files via upload

---
+++ CardConsumer.java
@@ -0,0 +1,34 @@
+package reststudy;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class CardConsumer {
+
+	private static final String endpoint = "https://api.magicthegathering.io/v1/cards/";
+	
+	@Bean
+	public RestTemplate restTemplate() {
+		RestTemplate restTemplate = new RestTemplate();
+		return restTemplate;
+	}
+	
+	public List<Card> getCards(){
+		HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
+        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+        ResponseEntity<CardWrapper> resp = restTemplate().exchange(endpoint, HttpMethod.GET,entity,CardWrapper.class);
+		return resp.getBody().getCards();
+	}
+}
---
+++ Start.java
@@ -0,0 +1,16 @@
+package reststudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("reststudy")
+public class Start {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Start.class, args);
+
+	}
+
+}
