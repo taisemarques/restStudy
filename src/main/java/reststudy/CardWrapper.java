b2e0c9a5010b187cdd8b7150881b69863ddf2be5
taisemarques <taise.marques@hotmail.com>
2018-09-13 12:20:29 -0300

    magic

---
+++ src/main/java/reststudy/CardWrapper.java
@@ -0,0 +1,17 @@
+package reststudy;
+
+import java.util.ArrayList;
+
+public class CardWrapper {
+
+	private ArrayList<Card> cards;
+
+	public ArrayList<Card> getCards() {
+		return cards;
+	}
+
+	public void setCard(ArrayList<Card> cards) {
+		this.cards = cards;
+	}
+
+}
